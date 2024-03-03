04005cd6fb77702a9535e92375788f0d1c847296
chun <zhengchh6@mail2.sysu.edu.cn>
2024-03-03 15:51:14 +0800

    m

---
+++ prepare_dataset.py
@@ -0,0 +1,14 @@
+# Prepare superpixels dataset for image dataset using SLIC algorithm
+# https://scikit-image.org/docs/dev/api/skimage.segmentation.html#skimage.segmentation.slic
+# this script refers to https://github.com/graphdeeplearning/benchmarking-gnns/data/superpixels
+# and https://github.com/bknyaz/graph_attention_pool/extract_superpixels.py
+
+
+def 
+
+
+
+
+
+if __name__ == '__main__':
+    main()
