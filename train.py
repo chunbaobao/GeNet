c72409cd80dd099a269d9e88de90dc18c986a5f1
chun <zhengchh6@mail2.sysu.edu.cn>
2024-03-06 18:29:39 +0800

    m

--- train.py
+++ train.py
@@ -20,6 +20,54 @@ from channel import Channel
 
 
 
+def train_epoch_sparse(model, optimizer, device, data_loader, epoch):
+    model.train()
+    epoch_loss = 0
+    epoch_train_acc = 0
+    nb_data = 0
+    gpu_mem = 0
+    for iter, (batch_graphs, batch_labels) in enumerate(data_loader):
+        batch_graphs = batch_graphs.to(device)
+        batch_x = batch_graphs.ndata['feat'].to(device)  # num x feat
+        batch_e = batch_graphs.edata['feat'].to(device)
+        batch_labels = batch_labels.to(device)
+        optimizer.zero_grad()
+        
+        batch_scores = model.forward(batch_graphs, batch_x, batch_e)
+        loss = model.loss(batch_scores, batch_labels)
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.detach().item()
+        epoch_train_acc += accuracy(batch_scores, batch_labels)
+        nb_data += batch_labels.size(0)
+    epoch_loss /= (iter + 1)
+    epoch_train_acc /= nb_data
+    
+    return epoch_loss, epoch_train_acc, optimizer
+
+def evaluate_network_sparse(model, device, data_loader, epoch):
+    model.eval()
+    epoch_test_loss = 0
+    epoch_test_acc = 0
+    nb_data = 0
+    with torch.no_grad():
+        for iter, (batch_graphs, batch_labels) in enumerate(data_loader):
+            batch_graphs = batch_graphs.to(device)
+            batch_x = batch_graphs.ndata['feat'].to(device)
+            batch_e = batch_graphs.edata['feat'].to(device)
+            batch_labels = batch_labels.to(device)
+            
+            batch_scores = model.forward(batch_graphs, batch_x, batch_e)
+            loss = model.loss(batch_scores, batch_labels) 
+            epoch_test_loss += loss.detach().item()
+            epoch_test_acc += accuracy(batch_scores, batch_labels)
+            nb_data += batch_labels.size(0)
+        epoch_test_loss /= (iter + 1)
+        epoch_test_acc /= nb_data
+        
+    return epoch_test_loss, epoch_test_acc
+
+
 def config_parser():
     import argparse
     parser = argparse.ArgumentParser()
