89baca7d660dfc092e063d9ec096d331ecfbd728
chun <zhengchh6@mail2.sysu.edu.cn>
2024-03-08 14:28:29 +0800

    m

--- channel.py
+++ channel.py
@@ -8,6 +8,12 @@ class Channel(nn.Module):
         self.snr = snr
 
     def forward(self, z_hat: torch.Tensor):
-        pass
+        # z_hat : (batch_size, num_nodes, feature_dim)
+        k = torch.prod(z_hat.shape[1:])
+        sig_pwr = torch.sum(torch.abs(z_hat).square(), dim=1, keepdim=True) / k
+        noi_pwr = sig_pwr / ( 10 ** (self.snr / 10))
+        noise = torch.randn_like(z_hat) * torch.sqrt(noi_pwr)
+        return z_hat + noise
+
 
         # return z_hat + noise
